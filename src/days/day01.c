629c5baab54c468906bd1120ef0dad01fbb66c38
Riley Grant <grantrileyh@gmail.com>
2025-12-01 07:28:08 -0500

    feat: solve day 01 part 2

--- src/days/day01.c
+++ src/days/day01.c
@@ -12,27 +12,51 @@ int day01()
     int zeroes = 0;
     int curr = 50;
 
+    int parttwo = 0;
+
+    int spins = 0;
+    int remain = 0;
+
+    int dial = 100;
+
     while (fgets(buff, size, fptr)) {
         char dir = buff[0];
         int num = atoi(buff + 1);
 
+        spins = num / dial; 
+        remain = num % dial; 
+
+        parttwo = parttwo + spins;
+
         if (dir == 'L') {
+
+            if (remain > curr && curr != 0) {
+                parttwo = parttwo + 1;
+            }
+
             curr = curr - num;
+
             if (curr < 0) {
                 curr = curr % 100;
-                curr = curr + 100;
+                if (curr != 0) {
+                    curr = curr + 100;
+                }
             }
-            printf("\nRotated L%d to %d", num, curr);
+            
         } else if (buff[0] == 'R') {
+
+            if (remain > (100 - curr) && curr != 0) {
+                parttwo = parttwo + 1;
+            }
+
             curr = curr + num;
+
             if (curr > 99) {
                 curr = curr % 100;
             }
-            printf("\nRotated R%d to %d", num, curr);
         }
 
-        if (curr == 0 || curr == 100) {
-            printf("  <-- incrementing!");
+        if (curr == 0) {
             zeroes = zeroes + 1;
         }
 
@@ -42,6 +66,7 @@ int day01()
 
     printf("\n\nDay 01: \n-----");
     printf("\n  Part 1: %d", zeroes); // 1071 correct
+    printf("\n  Part 2: %d\n", zeroes + parttwo); // 6700 correct
 
     return 0;
 }
