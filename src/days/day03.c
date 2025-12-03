02e2b40634cb2a27ab422e32e8ea11d239c4eeb1
Riley Grant <grantrileyh@gmail.com>
2025-12-03 08:49:04 -0600

    feat: solve day 03 part 2

--- src/days/day03.c
+++ src/days/day03.c
@@ -21,12 +21,16 @@ int day03() {
     long long part1 = 0;
     int double_digit = 0;
 
+    long long part2 = 0;
+
     while (fgets(buffer, line_size, fptr)) {
         int curr_highest = 0;
 
         buffer[strcspn(buffer, "\n")] = '\0';
         int line_length = strlen(buffer);
 
+        // Part 1
+        // ---
         for(int i = 0; i < line_length; i++) {
             for (int j = i + 1; j < line_length; j++) {
                 double_digit = create_double_digit(buffer[i], buffer[j]);
@@ -36,15 +40,37 @@ int day03() {
             }
         }
         part1 += curr_highest;
+
+        // Part 2 
+        // ---
+        long long curr_number = 0;
+        int window = line_length - 11;
+        int index = 0;
+        int offset;
+
+        while (curr_number <= 99999999999) {
+            int highest_digit = 0;
+
+            for(int k = 0; k < window; k++) {
+                int digit = buffer[index + k] - '0';
+                if (digit > highest_digit) {
+                    highest_digit = digit;
+                    offset = k;
+                }
+            }
+            curr_number = (curr_number * 10) + highest_digit;
+            window -= offset;
+            index += offset + 1;
+        }
+
+        part2 += curr_number;
+
     }
 
     printf("\n\nDay 03: \n-----");
     printf("\n  Part 1: %lld", part1); // 17359 correct 
-    printf("\n  Part 2: %lld", (long long)0); // 21932258645 correct
+    printf("\n  Part 2: %lld", part2); // 172787336861064 correct
     printf("\n");
 
     return 0;
 }
-
-
-
