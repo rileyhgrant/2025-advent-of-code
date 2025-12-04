2162264880b07c77d32c7e78dfb6ffb617f4256b
Riley Grant <grantrileyh@gmail.com>
2025-12-04 07:36:07 -0600

    feat: solve day 04 part 1

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "day01.h"
 #include "day02.h"
 #include "day03.h"
+#include "day04.h"
 
 
 // Run with make run day=<DAY>
@@ -20,6 +21,7 @@ int main(int argc, char **argv) {
         case 1: day01(); break;
         case 2: day02(); break;
         case 3: day03(); break;
+        case 4: day04(); break;
         default: printf("Day %d not found", day); break;
     }   
 }
